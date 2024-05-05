fcdaabdf832ca52fa9d634fc2baa33b3bd58c691
vineet-v22 <vermavineet418@gmail.com>
2024-05-06 02:26:46 +0530

    second commit

--- backend/controllers/orderControllers.js
+++ backend/controllers/orderControllers.js
@@ -4,8 +4,23 @@ const ErrorHandler = require("../utils/errorhandler");
 const catchAsyncErrors = require("../middleware/catchAsyncErrors");
 
 // Create New Order
-exports.newOrder = catchAsyncErrors((req,res,next)=>{
-    const {shippingInfo,orderItems,paymentInfo,itemsPrice,taxPrice,shippingPrice,totalPrice} = req.body;
-})
+exports.newOrder = catchAsyncErrors(async (req, res, next) => {
+    const { shippingInfo, orderItems, paymentInfo, itemsPrice, taxPrice, shippingPrice, totalPrice } = req.body;
 
-const order = await 
+    const order = await Order.create({
+        shippingInfo,
+        orderItems,
+        paymentInfo,
+        itemsPrice,
+        taxPrice,
+        shippingPrice,
+        totalPrice,
+        paidAt: Date.now(),
+        user: req.user._id,
+    });
+
+    res.status(201).json({
+        success: true,
+        order,
+    });
+});
